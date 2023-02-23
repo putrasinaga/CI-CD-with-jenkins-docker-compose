cdcee5f1d91ee6e4f8ddf4e8d7bbcccbec265f84
Putra Sinaga <putrasaut@gmail.com>
2023-02-23 20:55:29 +0700

    add file main.go

--- main_test.go
+++ main_test.go
@@ -1,32 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
 )
-
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World putra!!")
-}
-
-func Endointsaya(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "ini adalah endpoint saya!")
-}
-
-func main() {
-	http.HandleFunc("/", HelloServer)
-	http.HandleFunc("/saya", Endointsaya)
-	err := http.ListenAndServe(":3000", nil)
-
-	if err != nil {
-
-		panic(err)
-
-	}
-}
 
 func TestHelloServer(t *testing.T) {
 	req, err := http.NewRequest("GET", "/", nil)
