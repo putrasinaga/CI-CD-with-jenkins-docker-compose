42bf84df755ad42e36204fb391d08da658f1edfc
Putra Sinaga <37802827+putrasinaga@users.noreply.github.com>
2023-03-13 18:54:07 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,26 +1,62 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START gae_go111_app]
+
+// Sample helloworld is an App Engine app.
 package main
 
+// [START import]
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World putra!!")
-}
-
-func Endointsaya(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "ini adalah endpoint saya!")
-}
+// [END import]
+// [START main_func]
 
 func main() {
-	http.HandleFunc("/", HelloServer)
-	http.HandleFunc("/saya", Endointsaya)
-	err := http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/", index)
 
-	if err != nil {
-
-		panic(err)
-
+	// [START setting_port]
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Default port nya adalah %s", port)
 	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+	// [END setting_port]
 }
+
+// [END main_func]
+
+// [START indexHandler]
+
+// indexHandler responds to requests with our greeting.
+func index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Hello, World buddy!")
+}
+
+// [END indexHandler]
+// [END gae_go111_app]
